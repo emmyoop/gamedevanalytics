156a675b92e961004e4947545981519b4fc0fcfa
emmyoop <ebuschang@gmail.com>
2020-08-17 15:42:58 -0500

    on the flyu work in a notebook to pull data WIP

--- request_data.py
+++ request_data.py
@@ -1,1 +1,58 @@
-# API requests live here
+# standard library imports
+import csv
+import datetime as dt
+import json
+import os
+import statistics
+import time
+
+# third-party imports
+import numpy as np
+import pandas as pd
+import requests
+
+import settings
+
+# customisations - ensure tables show all columns
+pd.set_option("max_columns", 100)
+
+def get_request(url, parameters=None):
+    """Return json-formatted response of a get request using optional parameters.
+
+    Parameters
+    ----------
+    url : string
+    parameters : {'parameter': 'value'}
+        parameters to pass as part of get request
+
+    Returns
+    -------
+    json_data
+        json-formatted response (dict-like)
+    """
+    try:
+        response = requests.get(url=url, params=parameters)
+    except requests.exceptions.Timeout:
+        for i in range(5, 0, -1):
+            print('\rTimeout. Waiting... ({})'.format(i), end='')
+            time.sleep(1)
+        print('\rRetrying.' + ' ' * 10)
+        return get_request(url, parameters)
+    except requests.exceptions.TooManyRedirects:
+        print('TooManyRedirects')
+    except requests.exceptions.RequestException as e:
+    # catastrophic error. bail.
+        raise SystemExit(e)
+
+    if response:
+        return response.json()
+    else:
+        # response is none usually means too many requests. Wait and try again
+        print('No response, waiting 10 seconds...')
+        time.sleep(10)
+        print('Retrying.')
+        return get_request(url, parameters)
+
+
+if __name__ == "__main__":
+    json_data = get_request(settings.ALL_APPS_URL)
--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
 DATA_DIR = "data"
 PROCESSED_DIR = "processed"
+
+
+# API URLs
+ALL_APPS_URL = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json"
+APP_DETAILS_URL = "http://store.steampowered.com/api/appdetails?appids={APP_ID}"
